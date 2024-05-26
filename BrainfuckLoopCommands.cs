f8c87f97b780c914cfff3c36c748c49bb6b49181
Alex Tarski <Alex.F.L.O.W@yandex.ru>
2024-05-27 00:24:17 +0300

    Add loops feature

--- BrainfuckLoopCommands.cs
+++ BrainfuckLoopCommands.cs
@@ -1,13 +1,51 @@
+using System;
 using System.Collections.Generic;
 
 namespace func.brainfuck
 {
-	public class BrainfuckLoopCommands
-	{
-		public static void RegisterTo(IVirtualMachine vm)
-		{
-			vm.RegisterCommand('[', b => { });
-			vm.RegisterCommand(']', b => { });
-		}
-	}
+    public class BrainfuckLoopCommands
+    {
+        public static void RegisterTo(IVirtualMachine vm)
+        {
+            int[] bracketIndices = FindBracketIndices(vm.Instructions);
+
+            vm.RegisterCommand('[', b =>
+            {
+                if (b.Memory[b.MemoryPointer] == 0)
+                {
+                    b.InstructionPointer = bracketIndices[b.InstructionPointer];
+                }
+            });
+
+            vm.RegisterCommand(']', b =>
+            {
+                if (b.Memory[b.MemoryPointer] != 0)
+                {
+                    b.InstructionPointer = bracketIndices[b.InstructionPointer];
+                }
+            });
+        }
+
+        private static int[] FindBracketIndices(string instructions)
+        {
+            int[] indices = new int[instructions.Length];
+            Stack<int> stack = new Stack<int>();
+
+            for (int i = 0; i < instructions.Length; i++)
+            {
+                if (instructions[i] == '[')
+                {
+                    stack.Push(i);
+                }
+                else if (instructions[i] == ']')
+                {
+                    int openingBracket = stack.Pop();
+                    indices[openingBracket] = i;
+                    indices[i] = openingBracket;
+                }
+            }
+
+            return indices;
+        }
+    }
 }
